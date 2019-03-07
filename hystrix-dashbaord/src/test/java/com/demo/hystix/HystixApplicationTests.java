d0e65182b06d3af2e6156555106feb816c6e447b
zhangchao <chao.zhang@hypers.com>
2019-03-07 19:01:03 +0800

    update: add hystrix

---
+++ hystrix-dashbaord/src/test/java/com/demo/hystix/HystixApplicationTests.java
@@ -0,0 +1,16 @@
+package com.demo.hystix;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HystixApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
